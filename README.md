e89bbd6df758ae841f75d93a9bb66905c91079d1
alex-bci <alexandermorris@Alexanders-MacBook-Pro.local>
2018-05-30 23:47:40 -0500

    adding compiler files

---
+++ README.md
@@ -0,0 +1,44 @@
+
+localearn
+=================
+
+## Setup (one-time)
+
+### Linux/Debian/Ubuntu
+Ensure you have an updated version of nodejs and npm installed. Ubuntu repos often contain an old version, so it's recommend to use a more [up-to-date repo](https://launchpad.net/~chris-lea/+archive/node.js/).
+
+If you already had nodejs installed and need it updated, make sure to `sudo apt-get update && sudo apt-get upgrade`. Otherwise a `sudo apt-get install nodejs npm` should be sufficient.
+
+** on Ubuntu / Debian engines, there are minor issues with more recent versions of Gulp. To be safe, use package-old.json on these OS. 
+
+`sudo mv package-old.json package.json`
+
+### Mac
+`brew install node`
+
+### Windows
+Install node from http://nodejs.org/download/ and open the node command prompt.
+
+## Installation
+Assuming nodejs/npm is correctly installed, the next step is to install gulp with and then all the project dependencies:
+
+```
+sudo npm -g install gulp
+npm install
+```
+
+## Development
+
+To use the default dev task, which launches a local server for preview, run gulp:
+
+```
+gulp
+```
+
+This will compile the site to ./build/ (or whatever is defined as `basePaths.dest` in gulpfile.js). To instruct gulp to compile for production, pass `--prod` and possibly `gulp bundle` to compress it into a zip. For example:
+
+```
+gulp build --prod && gulp bundle
+```
+
+
---
+++ gulpfile.config.js
@@ -0,0 +1,107 @@
+
+var GLOBSTAR = '**/*';
+
+var basePaths = {
+    src: 'src/',
+    dest: 'docs/'
+};
+
+var typeMap = {
+    // templates
+    jade:       ['*.jade'],
+    html:       ['*.html'],
+
+    // styles
+    css:        [GLOBSTAR + '.css'],
+    less:       [GLOBSTAR + '.less'],
+
+    // scripts
+    js:         [GLOBSTAR + '.js', '!lib/*.js'],
+    coffee:     [GLOBSTAR + '.coffee'],
+    jslibs:     ['lib/*.js', '!lib/'],
+
+    // images
+    png:        [GLOBSTAR + '.png'],
+    jpg:        [GLOBSTAR + '.jpg', GLOBSTAR + '.jpeg'],
+    gif:        [GLOBSTAR + '.gif'],
+    svg:        [GLOBSTAR + '.svg'],
+
+    // extras
+    extras:   [GLOBSTAR]
+};
+
+module.exports = {
+  appName: 'blockchainwtfedu',
+  SERVER_PORT: 1338,
+  GLOBSTAR: GLOBSTAR,
+  basePaths: basePaths,
+
+
+  typePaths: {
+    templates: {
+      src: basePaths.src + 'templates/',
+      dest: basePaths.dest + ''
+    },
+    styles: {
+      src: basePaths.src + 'styles/',
+      dest: basePaths.dest + 'css/'
+    },
+    scripts: {
+      src: basePaths.src + 'scripts/',
+      dest: basePaths.dest + 'js/'
+    },
+    images: {
+      src: basePaths.src + 'images/',
+      dest: basePaths.dest + 'images/'
+    },
+    extras: {
+      src: basePaths.src + 'extras/',
+      dest: basePaths.dest + ''
+    },    
+    fonts: {
+      src: basePaths.src + 'styles/fonts/',
+      dest: basePaths.dest + 'css/fonts/'
+    },
+    i18n: {
+      src: basePaths.src + 'extras/locales',
+      dest: basePaths.dest + 'locales/'
+    }
+  },
+
+  typeMap: typeMap,
+
+  appFiles: {
+    templates:  typeMap.jade.concat(typeMap.html),
+    styles:     typeMap.css.concat(typeMap.less),
+    scripts:    typeMap.js.concat(typeMap.coffee),
+    images:     typeMap.png.concat(typeMap.gif).concat(typeMap.jpg).concat(typeMap.svg),
+    extras:     typeMap.extras
+  },
+
+  vendorFiles: {
+    scripts:    typeMap.jslibs
+  },
+
+  scriptOrder: [
+    'tether.min.js',
+    'jquery-2.2.3.min.js',
+    'jquery.main.js',
+    'jquery.flexisel.js',
+    'header-scroller.js',
+    'move-top.js',
+    'easing.js',
+    'bootstrap.min.js',
+    'main.js',
+
+ 
+  ],
+
+  styleOrder: [
+    'bootstrap.min.css',
+    'fontawesome-all.css',
+    'style',
+    'swipebox.css',
+    'custom.css',
+    'responsive.css'
+  ]
+};
